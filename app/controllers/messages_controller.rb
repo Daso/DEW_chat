932fd5f4925dac9713668bfaa1b355cfcedc66a6
Daniel Subauste <daso@.Galaxy>
2012-02-02 14:51:45 -0500

    remote 2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -44,7 +44,7 @@ class MessagesController < ApplicationController
 
     respond_to do |format|
       if @message.save
-        format.html { redirect_to message_path, notice: 'Message was successfully created.' }
+        format.html { redirect_to @message, notice: 'Message was successfully created.' }
         format.json { render json: @message, status: :created, location: @message }
       else
         format.html { render action: "new" }
